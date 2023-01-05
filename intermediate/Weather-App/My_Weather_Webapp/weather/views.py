498f286326d34f2cf4295db540f0233e745d0932
hrrytech92 <webdevguy85@gmail.com>
2023-01-05 02:38:56 -0500

    feat: added the urls into weather app.

---
+++ intermediate/Weather-App/My_Weather_Webapp/weather/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+import requests
+from .models import City
+from .forms import CityForm
+# Create your views here.
+def index(request):
+    url = 'should_be_unique_for_you'
+    err_msg = ''
+    message = ''
+    message_class = ''
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if existing_city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'City does not exist!'
+            else:
+                err_msg = 'City already exists!'
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added Successfully!'
+            message_class = 'is_success'
+    
+    form = CityForm()
+    cities = City.objects.all()
+    weather_data = []
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+    context = {
+        'weather_data' : weather_data, 
+        'form' : form,
+        'message' : message,
+        'message_class' : message_class
+        }
+    return render(request,'weather/weather.html', context)
+def delete_city(requests, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
